9a0983a9c904ee7a8a2e3df7aa1fbcde4664a5c1
Kevin Lazarz <kevin@hafur.com>
2022-11-24 08:22:13 +0100

    updated local copy script

--- local/copy-library.sh
+++ local/copy-library.sh
@@ -2,6 +2,8 @@
 
 #scp /Users/kevin/Library.xml opc@130.61.182.191:.
 
-rsync -avzh Library.xml  opc@130.61.187.216:.
+#rsync -avzh /Users/kevin/Library.xml  opc@130.61.187.216:.
 
-rm /Users/kevin/Library.xml 
+mv /Users/kevin/Library.xml /Users/kevin/Documents/development/.
+
+#rm /Users/kevin/Library.xml 
